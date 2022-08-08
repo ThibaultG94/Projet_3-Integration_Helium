f1543f50ae3f7d4f6e638f23dcc0d66e036f7468
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-08-08 16:34:44 +0200

    Menu déroulant s'affiche (JavaScript)

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,14 @@
 const buildArrow = document.querySelector(".build");
 const buildContainer = document.querySelector(".build-nav");
+const buildBody = document.querySelectorAll("body > div:not(.build-nav)");
 
 buildArrow.addEventListener("click", () => {
-  buildArrow.classList.toggle("build-clicked");
-  console.log(buildContainer);
+  buildContainer.classList.toggle("build-clicked");
+});
+
+// if (condition) {
+// } else {
+// }
+buildBody.addEventListener("click", () => {
+  buildContainer.classList.add("build-declicked");
 });

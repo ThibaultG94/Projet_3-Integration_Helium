5b94f5d3eaca6ee62c558df2d4fe82580dc3b9f6
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-05 15:19:20 +0200

    Navbar responsive

---
+++ assets/scripts/navbar_smartphone.js
@@ -0,0 +1,31 @@
+const menuBurger = document.querySelector(".menuwhite");
+const menuSmartphone = document.querySelector(".navbar__smartphone--menu");
+const closeMenu = document.querySelector(".cross");
+const buildResponse = document.querySelector(".build__responsive");
+const useResponse = document.querySelector(".use__responsive");
+const resourcesResponse = document.querySelector(".resources__responsive");
+const buildResponseMenu = document.querySelector(".build__responsive--menu");
+const useResponseMenu = document.querySelector(".use__responsive--menu");
+const resourcesResponseMenu = document.querySelector(
+  ".resources__responsive--menu"
+);
+
+menuBurger.addEventListener("click", () => {
+  menuSmartphone.classList.add("clicked");
+});
+
+closeMenu.addEventListener("click", () => {
+  menuSmartphone.classList.remove("clicked");
+});
+
+buildResponse.addEventListener("click", () => {
+  buildResponseMenu.classList.toggle("response_clicked");
+});
+
+useResponse.addEventListener("click", () => {
+  useResponseMenu.classList.toggle("response_clicked");
+});
+
+resourcesResponse.addEventListener("click", () => {
+  resourcesResponseMenu.classList.toggle("response_clicked");
+});
